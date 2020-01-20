8622b724c2f15b626fdc75f83866d1e8b6fa66e8
Jeffreyo3 <Jeffrey.Orndorff@gmail.com>
2020-01-20 12:11:35 -0800

    Add insert post route

--- routes/posts-router.js
+++ routes/posts-router.js
@@ -31,8 +31,31 @@ router.get('/:id', (req, res) => {
         .catch(err => {
             console.log('get post by ID error: ', err)
             res.status(500).json({ success: false, errorMessage: "The post information could not be retrieved" });
+        });
+});
+
+//insert() pass a 'post' object and db will return object with ID
+router.post('/', (req, res) => {
+    const newPost = req.body;
+
+    if (!newPost.hasOwnProperty('title')) {
+        res.status(400).json({ errorMessage: "Please provide title for the post" })
+    }
+    if (!newPost.hasOwnProperty('contents')) {
+        res.status(400).json({ errorMessage: "Please provide contents for the post" })
+    }
+
+    db.insert(newPost)
+        .then(post => {
+            const addPost = { ...newPost, id: post.id }
+            res.status(201).json({ success: true, addPost });
         })
-})
+        .catch(err => {
+            console.log('insert post error: ', err);
+            res.status(500).json({ success: false, error: "There was an error while saving the post to the database" });
+        });
+});
+
 
 
 module.exports = router;
