dc0c910acd14287e1d70160d86755e411b6ca72f
Jeffreyo3 <Jeffrey.Orndorff@gmail.com>
2020-01-20 11:46:25 -0800

    Add findById route

--- routes/posts-router.js
+++ routes/posts-router.js
@@ -3,16 +3,36 @@ const db = require('../data/db');
 
 const router = express.Router();
 
+// find() containing all posts in database
 router.get('/', (req, res) => {
     db.find()
         .then(posts => {
             res.status(200).json(posts);
         })
         .catch(err => {
-            console.log('get posts, .find()', err)
-            res.status(500).json({success: false, errorMessage:"The post information could not be retrieved"});
+            console.log('get posts: ', err)
+            res.status(500).json({ success: false, errorMessage: "The post information could not be retrieved" });
         });
 });
+
+// findById() expects id & returns the post with the ID. If no matching ID, return empty array
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+
+    db.findById(id)
+        .then(post => {
+            if (post) {
+                res.status(200).json(post);
+            }
+            else {
+                res.status(404).json({ success: false, message: "The post with the specified ID does not exist." })
+            }
+        })
+        .catch(err => {
+            console.log('get post by ID error: ', err)
+            res.status(500).json({ success: false, errorMessage: "The post information could not be retrieved" });
+        })
+})
 
 
 module.exports = router;
